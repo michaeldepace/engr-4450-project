23f62be498e6bbf198510a4ba0b20b5fb7b871b8
Michael DePace <michaeldepacejr@gmail.com>
2023-11-07 14:02:04 -0500

    test

--- run_app.py
+++ run_app.py
@@ -3,5 +3,5 @@ from lsapp import create_app, socketio
 
 app = create_app()
 if __name__ == '__main__':
-    #socketio.run(app) #dev code
-    app.run() #production code
+    socketio.run(app) #dev code
+    #app.run() #production code
