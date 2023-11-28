79c63a0e64f4c9c79c22fed0f83c5d231c497d19
ellzie <19760814+ellzie@users.noreply.github.com>
2023-11-27 22:31:09 -0500

    commented up live.py

--- lsapp/live.py
+++ lsapp/live.py
@@ -9,15 +9,37 @@ from lsapp.s3 import connect_to_s3
 
 bp = Blueprint('live', __name__) # register/connect this script with the html template files and http url routes
 
+# The first decorator (bp.route) constructs the route that this function will be called for
+# In this instance, this is the leaderboard found at the site index, so the route is
+# simply / (from the url that is not needed to be specified as it can change)
+
+# Then login_required ensures that this route can only be accessed if the user
+# is currently logged in.
+# It basically just checks if a user is logged in and if not it redirects to 
+# a log in page
+
 @bp.route('/')
 @login_required
 def index():
+    # Lots of functions will have this line here
     db = get_db()
+    # This just creates a client to communicate with the data base
+
+    # Use said client to make calls to the data base, in this particular the
+    # function asks for it in descending order by like count, and only grabs 5
+    # then it checks the likes table to see which videos the current user has liked
+    # so it can correctly display which ones are liked and which can be liked
     leaderboard_vids = db.table("video_data").select("*").order("num_likes", desc=True).limit(5).execute().data
     like_data = db.table("video_likes").select("*").eq('user_id', g.user["usr_id"]).execute().data
+    # List of liked videos, this list will store the videos that the user liked
+    # in a way that we can use when we render the template
     liked_video_ids = []
     for item in like_data:
         liked_video_ids.append(item['vid_id'])
+    # render_template will be seen a lot, its the main call
+    # to render an html page using a template stored in the first argument
+    # the next few are variables needed for the template
+    # like what videos to display and which are liked
     return render_template('live/index.html', videos=leaderboard_vids, likes=liked_video_ids)
 
 @bp.route('/videos')
@@ -25,13 +47,20 @@ def index():
 #@cross_origin()
 def all_videos():
     db = get_db()
+    # this call sorts by creation date to show each video in order of most recent post
     vids = db.table("video_data").select("*").order('created_at', desc=True).execute().data
-    
+    # again, this like_data call will grab the videos this user liked
+    # and make it so they cannot like them again
     like_data = db.table("video_likes").select("*").eq('user_id', g.user["usr_id"]).execute().data
     liked_video_ids = []
     for item in like_data:
         liked_video_ids.append(item['vid_id'])
-
+    # These are all the comments stored in the comment database
+    # we then store each comment in a dictionary with its video in a list
+    # so essentially, each video id is a key in the dict
+    # each key has a empty list, which each comment gets appended to
+    # so a key will have a list with all the comment strings that then
+    # will get attched to the videos in the template
     comment_data = db.table("comment_data").select("*").execute().data
     comment_dictionary = {}
     for record in vids:
@@ -47,19 +76,26 @@ def all_videos():
 @login_required
 def submit():
     db = get_db()
+    # Now we have connect_to_s3 which uses boto3 to open a connection with the S3 Server
+    # this connection allows us to send videos to the server to be stored
     s3 = connect_to_s3()
     if request.method == "POST":
+        # If statement ensures the user has sent a file, otherwise ask for a file
         uploaded_video = request.files['file']
+        # Checking the file object size in bytes
+        # basically just go from start to end and thats how many bytes it is
         upload_filesize = uploaded_video.seek(0, 2)
+        # this sets the pointer to the front of the file, so that it can continue to read files
         uploaded_video.seek(0, os.SEEK_SET)
-
+        # Secure_filename checks the filename, ensures its not constructed in a way that can cause problems
+        # such as /../../../
         upload_filename = secure_filename(uploaded_video.filename)
 
         # check if video file size is creater than 30 mb limit
         if upload_filesize > 30000000: 
             flash('The uploaded file is too big. 30 mb upload limit, please try again.')
             return redirect(url_for('live.submit'))
-
+        # check if video file exists at all and isnt an empty file
         if upload_filesize == 0:
             flash('The uploaded file is not valid. Please try again.')
             return redirect(url_for('live.submit'))
@@ -73,11 +109,12 @@ def submit():
         if '.' not in upload_filename or upload_filename.rsplit('.', 1)[1].lower() != 'mp4': #not in current_app.config["UPLOAD_EXTENSIONS"]:
             flash('Incorrect file type. MP4 files only, please try again.')
             return redirect(url_for('live.submit'))
-
+        # Get upload time
         timestamp = datetime.now().strftime("%d%m%Y%H%M%S%f")
         user_id = g.user["usr_id"]
+        # craft the file name withh the user id and upload time
         file_name = f'vid-{user_id}-{timestamp}.mp4'
-
+        # Try and send the file to the bucket, specifying that it is mp4 video again and update the table of videos with the new video
         try:
             s3.meta.client.upload_fileobj(uploaded_video, 'engr-4450-fp', file_name, ExtraArgs={"ContentType": "video/mp4"}) # push the file data to the s3 bucket
             db.table("submissions").insert({"uploader_id": user_id, "video_s3_path": file_name}).execute() # log uploaded video in submissions db table
@@ -91,6 +128,8 @@ def submit():
 @bp.route('/video/<vid_id>', methods=["GET"]) #view and individual video on its own page
 @login_required
 def view_video(vid_id):
+    # EXTREMELY similar to all_videos, but instead only gets the one video and makes sure
+    # that video block doesnt have a link to itself, as it is on its own page
     db = get_db()
     vids = db.table("video_data").select("*").eq('vid_id', vid_id).execute().data
     
@@ -115,6 +154,11 @@ def view_video(vid_id):
 @bp.route('/like/<video_id>', methods=["POST"])
 @login_required
 def like_video(video_id):
+    # this function finds the video that the user is attempting to like
+    # if it has no likes, itll create the format for a liked video
+    # i.e. video id and the user id who likes it is appended
+    # in the like table it stores a list of each user who liked it
+    # so that users who liked a video do not like it again
     db = get_db()
     likes = db.table("video_likes").select('*', count='exact').eq('vid_id', video_id).eq('user_id', g.user["usr_id"]).execute().count
     if likes == 0:
@@ -125,6 +169,7 @@ def like_video(video_id):
 @bp.route('/unlike/<video_id>', methods=["POST"])
 @login_required
 def unlike_video(video_id):
+    # same deal here just slightly different because now if the video has no likes we an actully get rid of the entry
     db = get_db()
     likes = db.table("video_likes").select('*', count='exact').eq('vid_id', video_id).eq('user_id', g.user["usr_id"]).execute().count
     if likes != 0:
@@ -135,6 +180,9 @@ def unlike_video(video_id):
 @bp.route('/video/comment', methods=["POST"])
 @login_required
 def submit_video_comment():
+    # similarly to liking a video, this goes to the comment table, and adds an entry into
+    # the comment dictionary entry for the video id in the request, then
+    # redirect(request.referrer) sends the user back where they came
     db = get_db()
     user_id = request.form['user_id']
     vid_id = request.form['vid_id']
@@ -146,9 +194,15 @@ def submit_video_comment():
 @login_required
 def view_profile(user_id):
     db = get_db()
-    #print("-------------------", g.user['usr_id'], user_id)
+    # quick check here, if a user clicks on the My Profile button, they go to
+    # /myprofile
+    # but if they click on a name on the all_videos or index page
+    # they go to /profile/user_id
+    # so you could go to your profile page assuming you arent you
+    # so this ensures that if you attempt to go to your profile that isnt
+    # /myprofile
+    # you will be taken there instead
     if int(g.user['usr_id']) == int(user_id):
-        
         return redirect(url_for('auth.profile'))
 
     uploaded_vids = db.table("video_data").select("*").eq('uploader_id', user_id).execute().data
@@ -175,7 +229,7 @@ def view_profile(user_id):
         comment_dictionary[comment_vid_id].append(record)
 
     user_profile_data = db.table("users").select("*").eq('usr_id', user_id).execute().data[0]
-    user_profile_data["usr_created_at"] = str(user_profile_data["usr_created_at"])[:10]#.strftime('%m/%d/%Y, %H:%M:%S')
+    user_profile_data["usr_created_at"] = str(user_profile_data["usr_created_at"])[:10]
 
     #this query only grabs videos from the submissions table that have been liked by the current user
     liked_video_data = db.table("video_data").select('*, video_likes(user_id)').eq('video_likes.user_id', user_id).not_.is_('video_likes', 'null').execute().data 
